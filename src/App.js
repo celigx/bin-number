c23344f876c7f36e2b3e8a3621316e80f95d73d6
Celig <cela@protonmail.com>
2021-08-05 17:06:53 +0200

    Add input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.sass';
 import Card from './components/Card';
 
 function App() {
-  const [bin, setBin] = useState('3775');
+  const [bin, setBin] = useState('');
+
+  useEffect(() => {
+    if (bin.length >= 4) {
+      fetchBin();
+    }
+  }, [bin])
 
   const fetchBin = () => {
     fetch(`https://lookup.binlist.net/${bin}`)
@@ -13,9 +19,14 @@ function App() {
       })
   }
 
+  const handleChange = (e) => {
+    setBin(e.target.value)
+  }
+
   return (
     <div className="app">
       <Card bin={bin} />
+      <input type="number" maxLength='6' value={bin} onChange={handleChange} />
     </div>
   );
 }
