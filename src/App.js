68df0693f638a9c200a4a5a2d4ef404f2ca5f741
Celig <cela@protonmail.com>
2021-08-05 17:05:18 +0200

    Add fetch api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import './App.sass';
 import Card from './components/Card';
 
 function App() {
+  const [bin, setBin] = useState('3775');
+
+  const fetchBin = () => {
+    fetch(`https://lookup.binlist.net/${bin}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+  }
+
   return (
     <div className="app">
-      <Card />
+      <Card bin={bin} />
     </div>
   );
 }
