fea862c49ea0341f1be32da4ea9531e75f52e1f7
Celig <cela@protonmail.com>
2021-08-10 23:43:20 +0200

    Add title

--- src/App.js
+++ src/App.js
@@ -49,56 +49,64 @@ function App() {
   return (
     <div className="app">
 
-      <Card 
-        bin={bin}
-        flag={binData.flag}
-        country={binData.country}
-        scheme={binData.scheme}
-      />
+      <div className="titleContainer">
+        <h1 className="title">BIN CHECKER</h1>
+      </div>
 
-      <div className="containerOutput">
+      <div className="cardContainer">
 
-        <div className="binInput">
-          <h6 className="binTitle">BANK IDENTIFICATION NUMBER (BIN)</h6>
-          <input type="tel" maxLength='6' value={bin} onChange={handleChange} placeholder='Enter the first 6 digits of the card' />
-        </div>
-        
-        <div className="firstRow flex output">
-          <div className="country">
-            <h6 className="title">COUNTRY</h6>
-            <h5 className="text">{binData.country}</h5>
+        <Card 
+          bin={bin}
+          flag={binData.flag}
+          country={binData.country}
+          scheme={binData.scheme}
+        />
+
+        <div className="containerOutput">
+
+          <div className="binInput">
+            <h6 className="binTitle">BANK IDENTIFICATION NUMBER (BIN)</h6>
+            <input type="tel" maxLength='6' value={bin} onChange={handleChange} placeholder='Enter the first 6 digits of the card' />
           </div>
-          <div className="bank">
-            <h6 className="title">BANK</h6>
-            <h5 className="text">{binData.bank}</h5>
+          
+          <div className="firstRow flex output">
+            <div className="country">
+              <h6 className="title">COUNTRY</h6>
+              <h5 className="text">{binData.country}</h5>
+            </div>
+            <div className="bank">
+              <h6 className="title">BANK</h6>
+              <h5 className="text">{binData.bank}</h5>
+            </div>
+          </div>
+
+          <div className="secondRow flex output">
+            <div className="country">
+              <h6 className="title">SCHEME/NETWORK</h6>
+              <h5 className="text">{binData.scheme}</h5>
+            </div>
+            <div className="bank">
+              <h6 className="title">BRAND</h6>
+              <h5 className="text">{binData.brand}</h5>
+            </div>
+          </div>
+
+          <div className="thirdRow flex output">
+            <div className="type">
+              <h6 className="title">TYPE</h6>
+              <h5 className="text">{binData.type}</h5>
+            </div>
+            <div className="prepaid">
+              <h6 className="title">PREPAID</h6>
+              <h5 className="text">{binData.prepaid === '-' ? '-' : binData.prepaid === true ? 'Yes' : 'No'}</h5>
+            </div>
+            <div className="luhn">
+              <h6 className="title">LUHN</h6>
+              <h5 className="text">{binData.luhn === '-' ? '-' : binData.luhn === true ? 'Yes' : 'No'}</h5>
+            </div>
           </div>
         </div>
 
-        <div className="secondRow flex output">
-          <div className="country">
-            <h6 className="title">SCHEME/NETWORK</h6>
-            <h5 className="text">{binData.scheme}</h5>
-          </div>
-          <div className="bank">
-            <h6 className="title">BRAND</h6>
-            <h5 className="text">{binData.brand}</h5>
-          </div>
-        </div>
-
-        <div className="thirdRow flex output">
-          <div className="type">
-            <h6 className="title">TYPE</h6>
-            <h5 className="text">{binData.type}</h5>
-          </div>
-          <div className="prepaid">
-            <h6 className="title">PREPAID</h6>
-            <h5 className="text">{binData.prepaid === '-' ? '-' : binData.prepaid === true ? 'Yes' : 'No'}</h5>
-          </div>
-          <div className="luhn">
-            <h6 className="title">LUHN</h6>
-            <h5 className="text">{binData.luhn === '-' ? '-' : binData.luhn === true ? 'Yes' : 'No'}</h5>
-          </div>
-        </div>
       </div>
 
     </div>
