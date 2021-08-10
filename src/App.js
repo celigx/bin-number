0c4ae1d14363222ba5b0f20429998d01adb69c88
Celig <cela@protonmail.com>
2021-08-10 23:52:28 +0200

    Add subtext
    Change font weight

--- src/App.js
+++ src/App.js
@@ -51,6 +51,7 @@ function App() {
 
       <div className="titleContainer">
         <h1 className="title">BIN CHECKER</h1>
+        <p className="subtext">Use the first 6 digits of any credit or debit card</p>
       </div>
 
       <div className="cardContainer">
