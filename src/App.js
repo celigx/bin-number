071486b76e4e3557b38de1506fbb187641b11277
Celig <cela@protonmail.com>
2021-08-11 16:42:42 +0200

    Create output component
    Move div to Output component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.sass';
 import Card from './components/Card';
 import Title from './components/Title';
 import Input from './components/Input';
+import Output from './components/Output';
+
 
 function App() {
   const [bin, setBin] = useState('');
@@ -57,49 +59,17 @@ function App() {
           scheme={binData.scheme}
         />
 
-        <div className="containerOutput">
+        <div className="inputOutputContainer">
 
           <Input
             bin={bin}
             setBin={setBin}
           />
           
-          <div className="firstRow flex output">
-            <div className="country">
-              <h6 className="title">COUNTRY</h6>
-              <h5 className="text">{binData.country}</h5>
-            </div>
-            <div className="bank">
-              <h6 className="title">BANK</h6>
-              <h5 className="text">{binData.bank}</h5>
-            </div>
-          </div>
+          <Output 
+            binData={binData} 
+          />
 
-          <div className="secondRow flex output">
-            <div className="country">
-              <h6 className="title">SCHEME/NETWORK</h6>
-              <h5 className="text">{binData.scheme}</h5>
-            </div>
-            <div className="bank">
-              <h6 className="title">BRAND</h6>
-              <h5 className="text">{binData.brand}</h5>
-            </div>
-          </div>
-
-          <div className="thirdRow flex output">
-            <div className="type">
-              <h6 className="title">TYPE</h6>
-              <h5 className="text">{binData.type}</h5>
-            </div>
-            <div className="prepaid">
-              <h6 className="title">PREPAID</h6>
-              <h5 className="text">{binData.prepaid === '-' ? '-' : binData.prepaid === true ? 'Yes' : 'No'}</h5>
-            </div>
-            <div className="luhn">
-              <h6 className="title">LUHN</h6>
-              <h5 className="text">{binData.luhn === '-' ? '-' : binData.luhn === true ? 'Yes' : 'No'}</h5>
-            </div>
-          </div>
         </div>
 
       </div>
