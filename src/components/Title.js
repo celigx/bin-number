87f695be6642f3f07d8272f06a971bee0a16b9db
Celig <cela@protonmail.com>
2021-08-16 15:20:47 +0200

    Clean branch

--- src/components/Title.js
+++
@@ -1,8 +0,0 @@
-export default function Title() { 
-  return (
-    <div className="titleContainer">
-        <h1 className="title">BIN CHECKER</h1>
-        <p className="subtext">Use the first 6 digits of any credit or debit card</p>
-    </div>
-  )
-}
